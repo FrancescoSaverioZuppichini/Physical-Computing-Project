66d559f28afa364f3fbab541fef23f006f23e25e
FrancescoSaverioZuppichini <zuppif@usi.ch>
2018-04-20 17:19:56 +0200

    added basic server and react app

---
+++ core/cocktail/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+module.exports = router;
